2cc21d91c88495878be5090487c0c3120a988eaa
yana-kushmar <yana.puzevich@gmail.com>
2023-09-06 20:17:31 +0300

    port env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Response, Request} from "express"
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
